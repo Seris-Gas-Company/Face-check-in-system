0d357eb35ff576794570ffb61b21de1c0d93b73c
御泽 <129493047+swhyuze@users.noreply.github.com>
2023-10-20 20:03:13 +0800

    登录、注册选择界面

---
+++ program/main.py
@@ -0,0 +1,104 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
+import sys
+from myUI import Login_Form, Register_Form, Student_Form, Teacher_Form
+
+
+# 登录界面
+class My_Login_Form(QMainWindow, Login_Form.Ui_Login_Form):
+    def __init__(self, parent=None):
+        super(My_Login_Form, self).__init__(parent)
+        self.setupUi(self)
+
+    def login(self):
+        if len(self.lineEdit.text()) == 5 and self.lineEdit.text() == self.lineEdit_2.text():
+            print("jiaoshi")
+            controller.show_teacher()
+        elif len(self.lineEdit.text()) == 6 and self.lineEdit.text() == self.lineEdit_2.text():
+            print("xuesheng")
+            controller.show_student()
+        elif self.lineEdit.text() == 'admin' and self.lineEdit_2.text() == '000000':
+            print("admin")
+        else:
+            QMessageBox.information(self, "error!", "密码输入错误", QMessageBox.Ok)
+
+    def register(self):
+        controller.show_register()
+
+    def refindPassword(self):
+        QMessageBox.information(self, "error!", "请联系管理员18903200556", QMessageBox.Ok)
+
+
+# 学生界面
+class My_Student_Form(QMainWindow, Student_Form.Ui_Student_Form):
+    def __init__(self, parent=None):
+        super(My_Student_Form, self).__init__(parent)
+        self.setupUi(self)
+
+    # 教师界面
+
+
+class My_Teacher_Form(QMainWindow, Teacher_Form.Ui_Teacher_Form):
+    def __init__(self, parent=None):
+        super(My_Teacher_Form, self).__init__(parent)
+        self.setupUi(self)
+
+
+class My_Register_Form(QMainWindow, Register_Form.Ui_Register_Form):
+    def __init__(self, parent=None):
+        super(My_Register_Form, self).__init__(parent)
+        self.setupUi(self)
+
+    def register(self):
+        print("zhuce")
+
+    def back(self):
+        print("back")
+        controller.show_register_login()
+
+
+# 界面跳转
+class Controller:
+    def __init__(self):
+        self.login = My_Login_Form()
+        self.student = My_Student_Form()
+        self.teacher = My_Teacher_Form()
+        self.login.hide()
+        self.student.hide()
+        self.teacher.hide()
+        self.register = My_Register_Form()
+        self.login.hide()
+        self.register.hide()
+
+    def show_main(self):
+        self.login.show()
+
+    def show_student(self):
+        self.login.close()
+        self.student.show()
+
+    def show_teacher(self):
+        self.login.close()
+        self.teacher.show()
+
+    def show_register(self):
+        self.login.close()
+        self.register.show()
+
+    def show_register_login(self):
+        self.login.show()
+        self.register.close()
+
+    def show_teacher_login(self):
+        self.login.show()
+        self.teacher.close()
+
+    def show_student_login(self):
+        self.login.show()
+        self.student.close()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)  # 在 QApplication 方法中使用，创建应用程序对象
+    controller = Controller()
+    controller.show_main()
+    sys.exit(app.exec_())  # 结束进程，退出程序
---
+++ recognite_face.py
@@ -0,0 +1,58 @@
+import dlib
+from imageio import imread
+import glob
+import numpy as np
+
+detector = dlib.get_frontal_face_detector()
+predictor_path = 'shape_predictor_68_face_landmarks.dat'
+predictor = dlib.shape_predictor(predictor_path)
+face_rec_model_path = 'dlib_face_recognition_resnet_model_v1.dat'
+facerec = dlib.face_recognition_model_v1(face_rec_model_path)
+
+
+def get_feature(path):
+    img = imread(path)
+    dets = detector(img)
+    print('检测到了 %d 个人脸' % len(dets))
+    # 这里假设每张图只有一个人脸
+    shape = predictor(img, dets[0])
+    face_vector = facerec.compute_face_descriptor(img, shape)
+    return (face_vector)
+
+
+def distance(a, b):
+    a, b = np.array(a), np.array(b)
+    sub = np.sum((a - b) ** 2)
+    add = (np.sum(a ** 2) + np.sum(b ** 2)) / 2.
+    return sub / add
+
+
+path_lists1 = ["chenping.png", "vv1.jpg"]
+path_lists2 = ["vv2.png", "vvtest.png"]
+
+feature_lists1 = [get_feature(path) for path in path_lists1]
+feature_lists2 = [get_feature(path) for path in path_lists2]
+
+print("feature 1 shape", feature_lists1[0].shape)
+
+out1 = distance(feature_lists1[0], feature_lists1[1])
+out2 = distance(feature_lists2[0], feature_lists2[1])
+
+print("diff distance is", out1)
+print("same distance is", out2)
+
+out1 = distance(feature_lists1[0], feature_lists1[1])
+out2 = distance(feature_lists2[0], feature_lists2[1])
+
+
+def classifier(a, b, t=0.09):
+    if (distance(a, b) <= t):
+        ret = True
+    else:
+        ret = False
+    return (ret)
+
+print("vvtest is 张维为", classifier(feature_lists2[1], feature_lists2[1]))
+print("chenping is 张维为", classifier(feature_lists1[0], feature_lists2[1]))
+print("vv1 is 张维为", classifier(feature_lists1[1], feature_lists2[1]))
+print("vv2 is 张维为", classifier(feature_lists2[0], feature_lists2[1]))
